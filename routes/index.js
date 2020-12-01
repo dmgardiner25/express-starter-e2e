5b2c981922bee0c0ed4182d7b1908f3321f0ac7c
David Gardiner <dmgardiner25@gmail.com>
2020-12-01 22:00:15 +0000

    Add new route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Harald\'s Codespace Test' });
 });
 
+router.get('/test', function(req, res, next) {
+  res.render('index', { title: 'E2E test route' });
+});
+
 module.exports = router;
